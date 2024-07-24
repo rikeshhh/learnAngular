a6452ef29b96e3e941a9be68eb0549f742bcef1e
Rikesh Shrestha <114804728+RikeshShresth@users.noreply.github.com>
2024-07-24 22:02:37 +0545

    lessonThree:diff between better and not better approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,12 @@ import { NavBarComponent } from './Navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [RouterOutlet, NavBarComponent],
+  styles: `h1{color:red;}`, //not better approach
+  template: `<h1>hello world</h1>
+    <p>hello world</p>`, //not better approach
+  templateUrl: './app.component.html', //better approach
+  styleUrl: './app.component.css', //better approach
 })
 export class AppComponent {
   title = 'my-first-angular-app';
