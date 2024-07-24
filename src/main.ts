02583be7f277008a99ada055ea7abc75dde59354
Rikesh Shrestha <114804728+RikeshShresth@users.noreply.github.com>
2024-07-24 21:35:16 +0545

    learn:nesting components in angular

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {NavBarComponent} from './app/Navbar/navbar.component';
+import { NavBarComponent } from './app/Navbar/navbar.component';
 
-bootstrapApplication(NavBarComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
