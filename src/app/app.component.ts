dfdcc408a0e25b450316461e40239113dee956b2
Rikesh Shrestha <114804728+RikeshShresth@users.noreply.github.com>
2024-07-25 22:16:54 +0545

    lean:property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,13 @@ import { NavBarComponent } from './Navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-first-angular-app';
+  imgUrl: string =
+    'https://cdn.pixabay.com/photo/2024/03/01/15/03/portal-8606707_1280.png';
+  isDisabled: boolean = true;
 }
